cd37c211d4d2928472c174ea0850e41a453887db
GFFritz <gabrielf.fritz@gmail.com>
2023-11-15 20:46:48 -0300

    Final Layout

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Sambanismo Landing Page by [Gabriel Fritz](https://github.com/GFFritz)
+## Landingpage developed using npm, Tailwindcss CLI and AlpineJS
+
+- Layout Designed by [Jonathan Viana](https://www.behance.net/dioyviana)
+
+### Requirements
+- NPM
+
+### To install need to run the following command
+- `npm i` or `npm install`
+
+### To Edit using Tailwindcss CLI need to run the following command
+- `npx tailwindcss -i input.css -o style.css --watch`
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '*.{html,js}'
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Unbounded', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        mainbg: '#050505',
+        primaryBg: '#232323',
+        secondaryBg: '#F5F5F5',
+        primary: '#FFB800',
+        'primary-alt': '#eaa900',
+        secondary: '#707070',
+        btn: '#52FF00',
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          xs: '369px',
+          md: '640px',
+          lg: '1100px',
+          xl: '1393px',
+        },
+      },
+      boxShadow: {
+        'alt': '0px 0px 17px 0px #52FF00',
+      },
+    },
+  },
+  plugins: [],
+}
+
